6b5e7542ada8cf8c4d3fdb6ced74b93bc92c64db
Sarthak Agrawal <67321408+sarthakagrawal6@users.noreply.github.com>
2026-05-07 18:11:32 +0530

    feat: Backend setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { cn } from "@/lib/utils";
 import { Toaster } from "@/components/ui/sonner";
 import { ClerkProvider } from "@clerk/nextjs";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { TRPCReactProvider } from "../trpc/client";
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
 export const metadata: Metadata = {
@@ -24,7 +25,9 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en" className={cn("h-full", "antialiased", inter.variable)}>
         <body className="min-h-full flex flex-col">
-          <TooltipProvider>{children}</TooltipProvider>
+          <TRPCReactProvider>
+            <TooltipProvider>{children}</TooltipProvider>
+          </TRPCReactProvider>
           <Toaster />
         </body>
       </html>
